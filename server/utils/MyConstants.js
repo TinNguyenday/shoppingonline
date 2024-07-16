7f6dc5e608b128e63f5dfcfb34348ec87eccee83
ThanhanSP <nguyenvuthanhan1@gmail.com>
2024-07-16 21:00:57 +0700

    abc

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'webnangcao.nnfl5vi.mongodb.net',
+    DB_USER: 'thanhan34502',
+    DB_PASS: 'thanhan11082003',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: '123456',
+    JWT_EXPIRES: '360000000000', // in milliseconds
+    EMAIL_USER: 'nguyenvuthanhan1@gmail.com', // gmail service
+    EMAIL_PASS: 'fcly pbby rlwh oyov'
+  };
+  module.exports = MyConstants;
